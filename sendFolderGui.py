fa32ec7a92470383da77397cf7ede754de6aaccf
deshdeepak1 <rkdeshdeepak1@gmail.com>
2020-08-21 15:09:01 +0530

    Updated gui.

--- sendFolderGui.py
+++ sendFolderGui.py
@@ -8,15 +8,73 @@ except:
     os.system('apt-get install python-tkinter')
     from tkinter import *
 
+try:
+    from unidecode import unidecode
+except:
+    os.system('pip3 install unidecode')
+    from unidecode import unidecode
+
 from tkinter import messagebox , filedialog
 from pyrogram import Client
+from telegram_upload import files
 
 API_ID=os.environ['API_ID']
 API_HASH=os.environ['API_HASH']
 
 loop=asyncio.get_event_loop()
 
+async def sendVid(username,path):
+  atr=files.get_file_attributes(path)
+  print(atr)
+  d=atr[0].duration
+  w=atr[0].w
+  h=atr[0].h
+  thumb = files.get_file_thumb(path)
+  caption = os.path.basename(path)
+  print(atr[0])
+  await client.send_video(username,path,caption=caption,duration=d,width=w,height=h,thumb=thumb,supports_streaming=True)
+
+async def sendDoc(username,path):
+    if files.get_file_mime(path)=='video':
+        await sendVid(username,path)
+    else:
+        await client.send_document(username,path)
+
+
+def upload (username,path,replacer):
+  if os.path.isdir(path) :
+    p=path.replace(replacer,'')
+    loop.run_until_complete(client.send_message(username,p,parse_mode=None))
+    print(path)
+    Files=os.listdir(path)
+    Files.sort()
+    for file in Files :
+      path=path+"/"+file
+      upload(username,path,replacer)
+      path= path.replace("/"+file,'')
+      if file is Files[-1] :
+          p=path.replace(replacer,'')         
+          loop.run_until_complete(client.send_message(username,p,parse_mode=None))
+          print(path)
+  else:
+    loop.run_until_complete(sendDoc(username,path))
+    print(path)
+
 class MainPage(Frame):
+
+    def send(self):
+        global client
+        
+        path=self.pathVar.get()
+        username=self.userVar.get()
+        try:
+            username=int(username)
+        except:
+            pass
+        username=1423220725
+        replacer=os.path.dirname(path)+'/'
+        upload(username,path,replacer)
+
 
     def chooseFolder(self):
         path=filedialog.askdirectory(title='Select Folder')
@@ -37,59 +95,27 @@ class MainPage(Frame):
         self.pathB = Entry(self.f1,textvariable=self.pathVar,font='arial 14',width=35,highlightbackground='black')
         self.pathB.pack(side=LEFT)
 
-        self.pathBtn = Button(self.f1,font='arial 14',text='Browse',width=10,highlightbackground='black',bg='light blue',command=self.chooseFolder)
-        self.pathBtn.pack(side=LEFT,padx=5)
+        self.pathBtn = Button(self.f1,font='arial 14',text='Browse',width=8,highlightbackground='black',bg='light blue',command=self.chooseFolder)
+        self.pathBtn.pack(side=LEFT,padx=4)
 
-        channels=[]
-        groups=[]
-        users=[]
-        
-        async def dial():
-            async for dialog in client.iter_dialogs():
-                if dialog.chat.type=="channel"  :
-                    channels.append(dialog.chat)
-                elif dialog.chat.type in ["group","supergroup"] :
-                    groups.append(dialog.chat)
-                elif dialog.chat.type == "private" and dialog.chat.first_name!=None:
-                    users.append(dialog.chat)
-        loop.run_until_complete(dial())
-        
         self.f2 = Frame(self)
         self.f2.pack(fill=X,pady=2)
 
-        self.meRb = Radiobutton(self.f2,text="ME",font='veranda 14')
-        self.meRb.pack(fill=BOTH)
+        self.userL = Label(self.f2,text='Enter username/id :  ',font='arial 14')
+        self.userL.pack(padx=5,side=LEFT)
 
+        self.userVar=StringVar()
+        self.userB = Entry(self.f2,textvariable=self.userVar,font='arial 14',width=20,highlightbackground='black')
+        self.userB.pack(padx=10,side=LEFT)
+        self.userVar.set('me')
+        
         self.f3 = Frame(self)
-        self.f3.pack(fill=BOTH)
+        self.f3.pack(fill=X,pady=2)
 
-        self.f3a = Frame(self.f3)
-        self.f3a.pack(fill=X,side=TOP)
+        self.sendB = Button(self.f3,text='Send',font='helvetica 14',bg='light blue',highlightbackground='grey',fg='green',width=8,command=self.send)
+        self.sendB.pack()
+
         
-        self.f3a1 = Frame(self.f3a)
-        scroll1 = Scrollbar(self.f3a1)
-        scroll1.pack(side=RIGHT,fill=Y)
-        self.f3a1.config(yscrollcommand=scroll1.set)
-        self.f3a1.pack(fill=Y,side=LEFT)
-        
-        self.f3a2 = Frame(self.f3a)
-        scroll2 = Scrollbar(self.f3a2)
-        scroll2.pack(side=RIGHT,fill=Y)
-        self.f3a1.config(yscrollcommand=scroll2.set)
-        self.f3a2.pack(fill=Y,side=LEFT)
-        
-        self.f3a3 = Frame(self.f3a)
-        scroll3 = Scrollbar(self.f3a3)
-        scroll3.pack(side=RIGHT,fill=Y)
-        self.f3a1.config(yscrollcommand=scroll3.set)
-        self.f3a3.pack(fill=Y,side=LEFT)
-
-        self.f3b = Frame(self.f3)
-        self.f3b.pack(fill=X,pady=3,side=BOTTOM)
-
-        self.sendB = Button(self.f3b,text='Send',font='helvetica 16',bg='light blue',highlightbackground='grey',fg='green',width=10)
-        self.sendB.pack(pady=5)
-
 class App(Tk):
 
         def __init__(self):
@@ -97,7 +123,7 @@ class App(Tk):
 
             Tk.__init__(self)
             self.title('Telegram Send Folder')
-            self.geometry('700x850')
+            self.geometry('700x130')
             self.resizable(0,0)
             self.config(bg='dark grey')
 
