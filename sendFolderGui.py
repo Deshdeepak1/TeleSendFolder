430e77a8df3ed235e668256a3a7ca77b9f86fe70
deshdeepak1 <rkdeshdeepak1@gmail.com>
2020-08-22 11:44:32 +0530

    updated gui file

--- sendFolderGui.py
+++ sendFolderGui.py
@@ -8,11 +8,6 @@ except:
     os.system('apt-get install python-tkinter')
     from tkinter import *
 
-try:
-    from unidecode import unidecode
-except:
-    os.system('pip3 install unidecode')
-    from unidecode import unidecode
 
 from tkinter import messagebox , filedialog
 from pyrogram import Client
@@ -60,18 +55,25 @@ def upload (username,path,replacer):
     loop.run_until_complete(sendDoc(username,path))
     print(path)
 
+class LoginPage(Frame):
+
+    def __init__(self,app):
+        Frame.__init__(self,app)
+        self.pack()
+
 class MainPage(Frame):
 
     def send(self):
         global client
         
         path=self.pathVar.get()
-        username=self.userVar.get()
+        u=self.userVar.get()
         try:
-            username=int(username)
+            username=int(u)
+            if u[0]=='+':
+                username=u
         except:
-            pass
-        username=1423220725
+            username=u
         replacer=os.path.dirname(path)+'/'
         upload(username,path,replacer)
 
@@ -80,15 +82,56 @@ class MainPage(Frame):
         path=filedialog.askdirectory(title='Select Folder')
         self.pathVar.set(path)
 
+    def switch(self):
+        global client
+        try:
+            loop.run_until_complete(client.disconnect())
+        except:
+            pass
+        print('Disconnected.')
+
+    def logout(self):
+        global client
+        try: 
+            loop.run_until_complete(client.log_out())
+        except:
+            pass
+        finally:
+            os.system('rm -f *.session*')
+            print('Logged out.')
+
+
+
     def __init__(self,app):
         global client
         Frame.__init__(self,app)
         self.config(bg='dark grey')
-
+        
+        self.f0 = Frame(self)
+        self.f0.pack(fill=X,pady=5)
+        
+        self.loginL = Label(self.f0,text='Logged in as: ',font='arial 12')
+        self.loginL.pack(side=LEFT)
+        
+        me=loop.run_until_complete(client.get_me())
+        try:
+            name=me['first_name']+' '+me['last_name'] 
+        except:
+            name=me['first_name']
+        self.nameL = Label(self.f0,text=name,font='arial 12')
+        self.nameL.pack(side=LEFT)
+        
+        
+        self.logoutB = Button(self.f0,text='Logout',font='helvetica 12',bg='light blue',highlightbackground='grey',fg='red',width=5,command=self.logout)
+        self.logoutB.pack(side=RIGHT)
+        
+        self.switchB = Button(self.f0,text='Switch', font='helvetica 12',bg='light blue',highlightbackground='grey',fg='green',width=5,command=self.switch)
+        self.switchB.pack(side=RIGHT)
+        
         self.f1 = Frame(self)
         self.f1.pack(fill=X,pady=10)
 
-        self.pathL = Label(self.f1,text='Enter path: ',font='arial 14')
+        self.pathL = Label(self.f1,text='Enter path',font='arial 14')
         self.pathL.pack(side=LEFT)
         
         self.pathVar=StringVar()
@@ -101,7 +144,7 @@ class MainPage(Frame):
         self.f2 = Frame(self)
         self.f2.pack(fill=X,pady=2)
 
-        self.userL = Label(self.f2,text='Enter username/id :  ',font='arial 14')
+        self.userL = Label(self.f2,text='Enter username/id/phone no.',font='arial 14')
         self.userL.pack(padx=5,side=LEFT)
 
         self.userVar=StringVar()
@@ -123,7 +166,7 @@ class App(Tk):
 
             Tk.__init__(self)
             self.title('Telegram Send Folder')
-            self.geometry('700x130')
+            self.geometry('700x170')
             self.resizable(0,0)
             self.config(bg='dark grey')
 
@@ -135,13 +178,16 @@ class App(Tk):
                 if loop.run_until_complete(client.connect()):
                     print('Connected')
                     mainPage=MainPage(self)
+                    client.iter_dialogs()
                     mainPage.pack(fill=BOTH,expand=True)
                 else:
+                    os.system('rm -f *.session*')
                     print('Not connected')
                     loginPage=LoginPage(self)
                     loginPage.pack(fill=BOTH,expand=True)
-            except e:
-                messagebox.showerror('Error',e)
+            except :
+                print('Error')
+                exit()
 
 
 if __name__=='__main__':
